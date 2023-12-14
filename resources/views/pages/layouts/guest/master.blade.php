cef80499f3cdfefbc727d213c99b44645f4156a9
Jodi Kurniawan <97623180+jodikrwn@users.noreply.github.com>
2023-12-15 01:25:49 +0700

    first commit

---
+++ resources/views/pages/layouts/guest/master.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+@include('pages.layouts.guest.components.head')
+
+<body>
+    @include('pages.layouts.guest.components.navbar')
+    @yield('content')
+</body>
+
+</html>
---
+++ resources/views/pages/welcome.blade.php
@@ -0,0 +1,8 @@
+@extends('pages.layouts.guest.master')
+
+@section('title')
+    {{ ucwords('Sistem rekomendasi pemilihan mahasiswa berprestasi politeknik negeri Batam') }}
+@endsection
+
+@section('content')
+@endsection
