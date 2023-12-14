3cb6dd5766e5db78a8cda222982ef6ef4f0b1e2a
Jodi Kurniawan <97623180+jodikrwn@users.noreply.github.com>
2023-12-15 02:13:00 +0700

    navbar

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -6,17 +6,32 @@
 
 @section('content')
     <div class="container">
-        <section>
+        <section class="pt-3 pt-md-4">
             <div class="row">
                 <div
-                    class="col-md-7"
+                    class="col-md-8"
                     style="border: 2px solid red"
                 >
-                    <div style="min-height: calc(100vh - 165px);">
-                        <h1>
-                            Sistem Rekomendasi Pemilihan
-                            MAHASISWA BERPRESTASI
-                            Politeknik Negeri Batam
+                    <div
+                        class="d-flex justify-content-md-end"
+                        style="min-height: calc(100vh - 195px);"
+                    >
+                        <h1
+                            class="d-inline-block pe-md-5"
+                            style="border: 2px solid red"
+                        >
+                            <span class="h2 d-block">
+                                Sistem Rekomendasi Pemilihan
+                            </span>
+                            <span
+                                style="font-family: 'Poetsen One', sans-serif;"
+                                class="fw-medium"
+                            >
+                                MAHASISWA BERPRESTASI
+                            </span>
+                            <span class="h2 d-block">
+                                Politeknik Negeri Batam
+                            </span>
                         </h1>
                     </div>
                 </div>
