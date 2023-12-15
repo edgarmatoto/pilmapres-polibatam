f7771d80019e9c31f508e701d271bc631dd33270
Jodi Kurniawan <97623180+jodikrwn@users.noreply.github.com>
2023-12-15 20:55:49 +0700

    mahasiswa home page

---
+++ resources/views/pages/mahasiswa/home/index.blade.php
@@ -0,0 +1,8 @@
+@extends('pages.layouts.mahasiswa.master')
+
+@section('title')
+    Home Page - Sistem Rekomendasi Pemilihan Mahasiswa Berprestasi Politeknik Negeri Batam
+@endsection
+
+@section('content')
+@endsection
