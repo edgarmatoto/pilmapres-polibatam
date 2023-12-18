cc1871857d710a199ca1c8630532c7c1b7b41e9f
Jodi Kurniawan <97623180+jodikrwn@users.noreply.github.com>
2023-12-19 01:36:09 +0700

    Register

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\Mahasiswa;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
 class RegisterController extends Controller
@@ -15,43 +17,53 @@ class RegisterController extends Controller
 
     public function store(Request $request)
     {
-        $rules = [
-            'nama'      => 'required|string|max:100|regex:/^[a-zA-Z\s]+$/',
-            'nim'       => 'required|numeric|max_digits:15',
-            'email'     => 'required|string|max:100|email',
-            'no_hp'     => 'required|numeric|max_digits:15',
-            'tanggal'   => 'required|numeric|between:1,31',
-            'bulan'     => 'required|numeric|between:1,12',
-            'tahun'     => 'required|numeric|digits:4',
-            'password'  => 'required|string|min:5|max:20|regex:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/',
-        ];
-        $messages = [
-            'nama.regex'        => 'Only accepts letters.',
-            'password.regex'    => 'Passwords must contain at least 1 uppercase letter, lowercase letter, and numbers.'
-        ];
-        $attributes = [
-            'nama'      => 'Nama Lengkap',
-            'nim'       => 'NIM',
-            'email'     => 'Email',
-            'no_hp'     => 'No Hp',
-            'tanggal'   => 'Tanggal',
-            'bulan'     => 'Bulan',
-            'tahun'     => 'Tahun',
-            'password'  => 'Password',
-        ];
-        $validator = Validator::make($request->all(), $rules, $messages, $attributes);
-        $validator->after(function ($validator) use ($request) {
-            $day    = $request->input('day');
-            $month  = $request->input('month');
-            $year   = $request->input('year');
+        try {
+            $rules = [
+                'nama'      => 'required|string|max:100|regex:/^[a-zA-Z\s]+$/',
+                'nim'       => 'required|numeric|max_digits:15|unique:mahasiswa',
+                'email'     => 'required|string|max:100|email|unique:mahasiswa',
+                'no_hp'     => 'required|numeric|max_digits:15|unique:mahasiswa',
+                'tanggal'   => 'required|numeric|between:1,31',
+                'bulan'     => 'required|numeric|between:1,12',
+                'tahun'     => 'required|date_format:Y',
+                'password'  => 'required|string|min:5|max:20|regex:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/',
+            ];
+            $messages = [
+                'nama.regex'        => 'Only accepts letters.',
+                'password.regex'    => 'Passwords must contain at least 1 uppercase letter, lowercase letter, and numbers.'
+            ];
+            $attributes = [
+                'nama'      => 'Nama Lengkap',
+                'nim'       => 'NIM',
+                'email'     => 'Email',
+                'no_hp'     => 'No Hp',
+                'tanggal'   => 'Tanggal',
+                'bulan'     => 'Bulan',
+                'tahun'     => 'Tahun',
+                'password'  => 'Password',
+            ];
+            $validator = Validator::make($request->all(), $rules, $messages, $attributes);
+            $validator->after(function ($validator) use ($request) {
+                $day    = $request->input('tanggal');
+                $month  = $request->input('bulan');
+                $year   = $request->input('tahun');
 
-            $date = checkdate($day, $month, $year);
-            if (!$date) $validator->errors()->add('tanggal', 'Invalid date in the given month and year.');
-        });
-        if ($validator->fails()) return back()->withErrors($validator)->withInput();
+                $date = checkdate($month, $day, $year);
+                if (!$date) $validator->errors()->add('tanggal', 'Invalid date in the given month and year.');
+            });
+            if ($validator->fails()) return back()->withErrors($validator)->withInput();
 
+            $data               = $request->except(['tanggal', 'bulan', 'tahun', 'password']);
+            $data['tgl_lahir']  = date('Y-m-d', strtotime("$request->tahun-$request->bulan-$request->tanggal"));
+            $data['password']   = Hash::make($request->password);
+            Mahasiswa::create($data);
 
-
-        dd($request->all());
+            return redirect()
+                ->route('welcome')
+                ->withSuccess('Selamat akun anda berhasil didaftarkan.');
+        } catch (\Throwable $th) {
+            return back()
+                ->withError('Akun anda tidak dapat didaftarkan, silahkan coba lagi nanti.');
+        }
     }
 }
