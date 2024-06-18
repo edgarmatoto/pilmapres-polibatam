caffda4eac420a2bc6b03dd36efd0f5c8aa2b784
Edgar <edgarmatoto34@gmail.com>
2024-06-18 11:40:06 +0700

    add styles to logo

--- resources/views/pages/layouts/mahasiswa/components/navbar.blade.php
+++ resources/views/pages/layouts/mahasiswa/components/navbar.blade.php
@@ -2,6 +2,7 @@
     <div class="container">
         <a href="{{ route('welcome') }}">
             <img
+                style="width: 70px;"
                 class="navbar-brand img-fluid logo"
                 src="{{ asset('assets/images/logo.jpeg') }}"
                 alt="logo_poltek"
